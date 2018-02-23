e723bd72a853fb5f5c4cd17d7251670b3224f308
sikern <simon.kern@capgemini.com>
2018-02-23 18:14:36 +0100

    background set

--- src/main/java/testpackage/TestSimpleApplication.java
+++ src/main/java/testpackage/TestSimpleApplication.java
@@ -2,15 +2,14 @@ package testpackage;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.asset.plugins.FileLocator;
-import com.jme3.input.ChaseCamera;
+import com.jme3.font.BitmapText;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
-import com.jme3.scene.shape.Cylinder;
 
 public class TestSimpleApplication extends SimpleApplication{
 
@@ -36,7 +35,10 @@ public class TestSimpleApplication extends SimpleApplication{
             //configuration
         flyCam.setEnabled(false);
 
-        assetManager.registerLocator("C:/tocozme/Entwicklungsumgebung_v006/workspaces_intelliJ/playground/springBootDemo/gs-spring-boot/oneWay2Mars/src/rsc/pictures/", FileLocator.class);
+            //this is adding a location
+        assetManager.registerLocator(System.getProperty("user.dir") + "/src/rsc/pictures/", FileLocator.class);
+
+            //binding keys
         inputManager.addMapping("Up",  new KeyTrigger(KeyInput.KEY_UP));
         inputManager.addMapping("Down",  new KeyTrigger(KeyInput.KEY_DOWN));
         inputManager.addMapping("Left",  new KeyTrigger(KeyInput.KEY_LEFT));
@@ -44,36 +46,43 @@ public class TestSimpleApplication extends SimpleApplication{
         inputManager.addListener(analogListener, new String[]{"Up", "Down","Left","Right"});
 
             //create scene
-        Box b = new Box(2, 2, 0); // create cube shape
+        Box b = new Box(5.5f, 5.5f, 0); // create cube shape
         Box c = new Box(1, 1, 0.5f); // create cube shape
 
-        movingBox = new Geometry("movingCircle", c);
+            //background
+        image = new Geometry("Box", b);  // create cube geometry from the shape
+        Material tripToMars = new Material(
+                assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        tripToMars.setTexture("ColorMap",
+                assetManager.loadTexture("image_mars.jpg"));
+            //control box by keys
+        movingBox = new Geometry("movingBox", c);
         Material defaulty = new Material(assetManager,
                 "Common/MatDefs/Misc/Unshaded.j3md");
         defaulty.setTexture("ColorMap", assetManager.loadTexture(
                 "cartoon_return_from_mars.jpg"));
         movingBox.setMaterial(defaulty);
 
-        image = new Geometry("Box", b);  // create cube geometry from the shape
-        Material tripToMars = new Material(
-                assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        tripToMars.setTexture("ColorMap",
-                assetManager.loadTexture("mars_human.jpg"));
-
-
+            //attach to sceneGraph
         image.setMaterial(tripToMars);                   // set the cube's material
-
+        image.move(0,1,0);
         rootNode.attachChild(image);              // make the cube appear in the scene
         rootNode.attachChild(movingBox);
 
-
+            // Display a line of text with a default font
+        guiNode.detachAllChildren();
+        guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
+        BitmapText helloText = new BitmapText(guiFont, false);
+        helloText.setSize(guiFont.getCharSet().getRenderedSize());
+        helloText.setText("Hello Mars");
+        helloText.setLocalTranslation(300, helloText.getLineHeight(), 0);
+        guiNode.attachChild(helloText);
     }
     /* Use the main event loop to trigger repeating actions. */
     @Override
     public void simpleUpdate(float tpf) {
 
-        // make the player rotate:
-        //image.rotate(1*tpf, 0, 0);
+        //your updates here
     }
 
 
